cc499f39bae8942302eeff2b5640003f894abc1b
nasra <naasironasra@gmail.com>
2025-05-14 10:31:45 +0300

    ...

--- fluffy_haven.py
+++ fluffy_haven.py
@@ -1,7 +1,29 @@
+#class innitialization with default  init
+
 class Pet:
-    pass
+    def speak(self):
+        print("sound made")
+        # return ("pet spoke")
+
+Rasmus = Pet()
+Rasmus.name = "Rasmus"
+print(Rasmus.name)
+print(Rasmus.speak())
+
+#simple class to represent a dog with modified init 
 class Dog:
-    pass
+    def __init__(self, name, breed, age):
+        self.name = name
+        self.breed = breed
+        self.age  = age       
+    
+    def speak(self):
+        return f"{self.name} says woof! woof!"
+    
+Koba = Dog("Koba", "Great Dane", 3)
+Koba.age = 4
+print(Koba.speak())
+print(Koba.age)
 
 class Cat:
     pass
